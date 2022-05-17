2f231d4ad324c2ff70d8b8678f39ce04a7c3a82f
ZQY <jackyzhaolelouch@Outlook.com>
2022-05-17 15:56:10 +0200

    search function added

--- app/src/main/java/com/example/flowsensor12/MainActivity.java
+++ app/src/main/java/com/example/flowsensor12/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.flowsensor12;
 
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
@@ -15,17 +16,20 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity {
     BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-    public int REQUEST_ENABLE_BT=1;
+    public int REQUEST_ENABLE_BT = 1;
     Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+//打开蓝牙
     public void BluetoothFunctionStart(View view) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -41,12 +45,51 @@ public class MainActivity extends AppCompatActivity {
             //检测是否开启了蓝牙功能，注意startActivity会开启子线程，无法在此语句后面直接判断蓝牙功能是否被开启
             if (!bluetoothAdapter.isEnabled()) {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent,REQUEST_ENABLE_BT);
-            }else {
-                Toast.makeText(context,"Bluetooth on", Toast.LENGTH_LONG).show();
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+            } else {
+                Toast.makeText(MainActivity.this, "Bluetooth on", Toast.LENGTH_SHORT).show();
             }
-            }else{
-            Toast.makeText(context,"Your device doesn't support bluetooth", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Your device doesn't support bluetooth", Toast.LENGTH_SHORT).show();
+        }
+    }
+    //开启蓝牙搜索
+    public void DeviceSearchOn(View view) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (!bluetoothAdapter.isDiscovering()) {
+            bluetoothAdapter.startDiscovery();
+            Toast.makeText(MainActivity.this, "searching", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(MainActivity.this, "still searching", Toast.LENGTH_SHORT).show();
+        }
+    }
+    //结束蓝牙搜索
+    public void DeviceSearchOff(View view) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (bluetoothAdapter.isDiscovering()) {
+            bluetoothAdapter.cancelDiscovery();
+            Toast.makeText(MainActivity.this, "searching off", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(MainActivity.this, "not searching", Toast.LENGTH_SHORT).show();
         }
     }
 }
