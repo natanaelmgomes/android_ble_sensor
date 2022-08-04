0eaf106dad9f7e92e33d6d707efae8bc29f5b77a
qingyu <jackyzhaolelouch@outlook.com>
2022-08-04 17:22:14 +0200

    Solving the problem of dynamic chart,still using MPAndroidChart, failed to use HelloChart and JFreeChart

--- app/src/main/java/com/example/flowsensor12/Graph.java
+++ app/src/main/java/com/example/flowsensor12/Graph.java
@@ -1,137 +1,82 @@
 package com.example.flowsensor12;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.graphics.Color;
-import android.media.metrics.Event;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
-public class Graph extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class Graph extends Activity
+{
+    LineChart lineChart;
+    private float[] data = new float[100];
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
-        initChart();
-        //EventManager.setEventListener(event);
+        lineChart = (LineChart) findViewById(R.id.chart1);
+        test();
+        //setData(data);
     }
-    /*public interface Event {
-        void onSomthingHappened(String msg);
+
+    private void test(){
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                data[i] = new Random().nextFloat()*10;
+                i++;
+                setData(data);
+            }
+        };
+        timer.schedule(timerTask,1000,500);
     }
-    private Event event = new Event() {
-        @Override
-        public void onSomthingHappened(String msg) {
-            int data = Integer.parseInt(msg);
-            Log.d("Graph", "onSomthingHappend: " + data);
+
+    private void setData(float[] voltage) {
+        lineChart.getDescription().setEnabled(false);
+        lineChart.setTouchEnabled(true);
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(true);
+        lineChart.setPinchZoom(true);
+        lineChart.setDoubleTapToZoomEnabled(true);
+        lineChart.setBackgroundColor(Color.BLACK);
+        List<Entry> entries = new ArrayList<>();
+        for(int i = 0; i < voltage.length; i++)
+        {
+            entries.add(new Entry(i, voltage[i]));
         }
-    };*/
+        LineDataSet dataSet = new LineDataSet(entries, "Voltage");
+        dataSet.setColor(Color.GREEN);
+        dataSet.setCircleColor(Color.GREEN);
 
-    private LineChart chart;
-    private void initChart() {
-        chart = findViewById(R.id.chart1);
-        chart.setBackgroundColor(Color.BLACK);
-        //Zoom & Drag
-        chart.setTouchEnabled(true);
-        chart.setDragEnabled(true);
-        chart.setScaleEnabled(true);
-        chart.setScaleXEnabled(true);
-        chart.setScaleYEnabled(true);
-        chart.setDrawGridBackground(false);
-        chart.setPinchZoom(true);
-        chart.setDoubleTapToZoomEnabled(true);
-        //Line Chart
-        LineData data = new LineData();
-        data.setValueTextColor(Color.RED);
-        chart.setData(data);
-        //Legend
-        Legend l = chart.getLegend();
-        l.setForm(Legend.LegendForm.LINE);
-        l.setTextColor(Color.WHITE);
-        // XAxis
-        XAxis xl = chart.getXAxis();
-        xl.setTextColor(Color.WHITE);
-        xl.setDrawGridLines(true);
-        xl.setAvoidFirstLastClipping(true);
-        // YAxis left
-        YAxis yl = chart.getAxisLeft();
-        yl.setTextColor(Color.WHITE);
-        // YAxis right
-        YAxis yr = chart.getAxisRight();
-        yr.setTextColor(Color.WHITE);
-            addEntry();
-    }
-    private void addEntry() {
-        LineData data = chart.getData();
-        // 每一个LineDataSet代表一条线，每张统计图表可以同时存在若干个统计折线，这些折线像数组一样从0开始下标。
-        // 本例只有一个，那么就是第0条折线
-        LineDataSet set = (LineDataSet) data.getDataSetByIndex(0);
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
 
-        // 如果该统计折线图还没有数据集，则创建一条出来，如果有则跳过此处代码。
-        if (set == null) {
-            set = createLineDataSet();
-            data.addDataSet(set);
-        }
-
-        // 先添加一个x坐标轴的值
-        // 因为是从0开始，data.getXValCount()每次返回的总是全部x坐标轴上总数量，所以不必多此一举的加1
-        //data.addXValue((data.getXValCount()) + "");
-
-        // 生成随机测试数
-        float f = (float) ((Math.random()) * 20 + 50);
-
-        // set.getEntryCount()获得的是所有统计图表上的数据点总量，
-        // 如从0开始一样的数组下标，那么不必多次一举的加1
-        Entry entry = new Entry(f, set.getEntryCount());
-
-        // 往linedata里面添加点。注意：addentry的第二个参数即代表折线的下标索引。
-        // 因为本例只有一个统计折线，那么就是第一个，其下标为0.
-        // 如果同一张统计图表中存在若干条统计折线，那么必须分清是针对哪一条（依据下标索引）统计折线添加。
-        data.addEntry(entry, 0);
-
-        // 像ListView那样的通知数据更新
-        chart.notifyDataSetChanged();
-
-        // 当前统计图表中最多在x轴坐标线上显示的总量
-        chart.setVisibleXRangeMaximum(5);
-
-        // y坐标轴线最大值
-        // mChart.setVisibleYRange(30, AxisDependency.LEFT);
-
-        // 将坐标移动到最新
-        // 此代码将刷新图表的绘图
-        //chart.moveViewToX(data.getXValCount() - 5);
-
-        // mChart.moveViewTo(data.getXValCount()-7, 55f,
-        // AxisDependency.LEFT);
+        LineData lineData = new LineData(dataSet);
+        lineChart.setData(lineData);
+        dataSet.notifyDataSetChanged();
+        lineData.notifyDataChanged();
+        lineChart.notifyDataSetChanged();
+        lineChart.invalidate();
     }
 
-    // 初始化数据集，添加一条统计折线，可以简单的理解是初始化y坐标轴线上点的表征
-    private LineDataSet createLineDataSet() {
 
-        LineDataSet set = new LineDataSet(null, "动态添加的数据");
-        set.setAxisDependency(YAxis.AxisDependency.LEFT);
-
-        // 折线的颜色
-        set.setColor(ColorTemplate.getHoloBlue());
-
-        set.setCircleColor(Color.WHITE);
-        set.setLineWidth(10f);
-        set.setCircleRadius(5f);
-        set.setFillAlpha(128);
-        set.setFillColor(ColorTemplate.getHoloBlue());
-        set.setHighLightColor(Color.GREEN);
-        set.setValueTextColor(Color.WHITE);
-        set.setValueTextSize(10f);
-        set.setDrawValues(true);
-        return set;
-    }
 
 }
